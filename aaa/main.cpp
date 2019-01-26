0e26b3711f47c7ed9fdd2489a779b1b011545f7a
Romi <motoras@gmail.com>
2019-01-26 10:36:26 +0200

    Initial commit

---
+++ aaa/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int max(int x,int y){
+        if(x>y){
+            return x;
+        }else{
+            return y;
+        }
+}
+int max3(int x,int y,int z){
+    if(x>y){
+        if(x>z){
+            return x;
+        }else{
+            return z;
+        }
+    }else{
+        if(y<z){
+            return z;
+        }else{
+            return y;
+        }
+    }
+}
+
+void print_even(int from, int to){
+    for(int i=from;i<=to;i+=7){
+        if(i %2==0){
+            cout << i << endl;
+        }
+    }
+}
+
+int main()
+{
+    cout <<  max3(12,3,78)  << endl;
+    cout <<  max3(12,333,78)  << endl;
+    print_even(10,1000000);
+    return 0;
+}
